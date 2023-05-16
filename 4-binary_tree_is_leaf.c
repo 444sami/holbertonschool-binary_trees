83a81bdc2941059ef5c839c54baccf98dd90cbea
Sami <5741@holbertonstudents.com>
2023-05-16 06:51:25 -0700

    segmentation fault fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,7 +6,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right == NULL && node->left == NULL)
+	if (node->right == NULL && node->left == NULL && node)
 	{
 		return (1);
 	}
