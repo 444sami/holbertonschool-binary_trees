9b36299971ab9f9d960986aa6cd50b26467d2a81
Sami <5741@holbertonstudents.com>
2023-05-16 08:16:27 -0700

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,12 +6,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	size_t left_height, right_height;
 
 	if (!tree)
 		return (0);
 
-	left_height = 1 + binary_tree_height(tree->left);
-	right_height = 1 + binary_tree_height(tree->right);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (left_height > right_height ? left_height : right_height);
 }
